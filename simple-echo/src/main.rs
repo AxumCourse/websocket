f7e53402bce6c0ca4478b6aedd012b3b269d4a16
axumrs <rolson867@gmail.com>
2024-01-27 17:07:30 +0800

    简单echo的实现

--- simple-echo/src/main.rs
+++ simple-echo/src/main.rs
@@ -8,18 +8,12 @@ use axum::{
     Router,
 };
 use tokio::net::TcpListener;
-use tracing::info;
 
 #[tokio::main]
 async fn main() {
-    if std::env::var_os("RUST_LOG").is_none() {
-        std::env::set_var("RUST_LOG", "simple_echo=debug");
-    }
-    tracing_subscriber::fmt::init();
-
     let listener = TcpListener::bind("0.0.0.0:56789").await.unwrap();
     let app = Router::new().route("/ws", get(websocket_handler));
-    info!("监听于 {}", listener.local_addr().unwrap());
+    println!("监听于 {}", listener.local_addr().unwrap());
     axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
@@ -33,15 +27,15 @@ async fn handle_socket(mut socket: WebSocket) {
     while let Some(Ok(msg)) = socket.recv().await {
         match msg {
             Message::Close(_) => {
-                info!("客户端断开连接");
+                println!("客户端断开连接");
                 break;
             }
             Message::Text(text) => {
-                info!("收到客户端文本消息：{}", text);
+                println!("收到客户端文本消息：{}", text);
                 // 向客户端原样发送收到的消息
                 socket.send(Message::Text(text)).await.unwrap();
             }
-            _ => info!("收到客户端消息：{:?}", msg),
+            _ => println!("收到客户端消息：{:?}", msg),
         };
     }
 }
